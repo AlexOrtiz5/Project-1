e76989f3f2bbde0d3421616ad32ac23e65181803
Alex Ortiz <alexortizcotto0@gmail.com>
2023-12-15 18:23:40 -0400

    Update Clean_data.py

--- Clean_data.py
+++ Clean_data.py
@@ -10,30 +10,21 @@ def clean_columns_name(data):
 def clean_lego_data(data):
     # Drop duplicates, if any
     data.drop_duplicates(inplace=True)
-
+    
     # Convert 'year' column to datetime if it's in string format
-    if isinstance(Lego_df['year'][0], str):
-        Lego_df['year'] = pd.to_datetime(Lego_df['year'], errors='coerce').dt.year
-
-    # Fill missing values in 'Star rating' and 'Number of reviews' with 0
-    Lego_df['star_rating'].fillna(0, inplace=True)
-    Lego_df['number_of_reviews'].fillna(0, inplace=True)
-
-    # Remove commas and convert columns to numeric types
-    Lego_df['Set Price'] = Lego_df['set_price'].round(2)
-    #Lego_df['number_of_reviews'] = pd.to_numeric(Lego_df['number_of_reviews'], errors='coerce')
-    #Lego_df['star_rating'] = Lego_df['star_rating'].str.replace(',', '.', regex=True).astype(float)
-
+    if isinstance(data['year'][0], str):
+        data['year'] = pd.to_datetime(data['year'], errors='coerce').dt.year
+    
     # Fill missing values in 'Star rating' and 'Number of reviews' with 0
     data['star_rating'].fillna(0, inplace=True)
     data['number_of_reviews'].fillna(0, inplace=True)
-
+    
     # Remove commas and convert columns to numeric types
     data['set_price'] = data['set_price'].round(2)
-
+    
     # Drop rows with missing 'Set Price' values
     data.dropna(subset=['set_price'], inplace=True)
-
+    
     return data
 
 # Function to clean the data on the second csv
